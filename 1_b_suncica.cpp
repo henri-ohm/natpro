43edfd68c36fed806e83e480f1b9f0f91239f9d7
Henri <miho@mail>
2020-10-11 12:22:01 +0200

    solved with differential array and fenwick tree

---
+++ 1_b_suncica.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<long long> fen;
+
+long long fen_sum(int index)
+{
+	long long sum = 0;
+	index++;
+	for(; index > 0; index -= index & (-index))
+	{
+		sum += fen[index];
+	}
+	return sum;
+}
+
+void fen_add(int index, long long val)
+{
+	index++;
+	for(; index <= n; index += index & (-index))
+	{
+		fen[index] += val;
+	}
+}
+
+int main()
+{
+	int n,q;
+	cin >> n >> q;
+	long long x;
+	long long diff[n];
+	long long t[n];
+	fen = vector<long long>(n + 1);
+	
+	
+	for(int i = 0; i < n; i++)
+	{
+		cin >> t[i];
+
+		diff[i] = i == 0 ? t[i] : t[i] - t[i - 1];
+		fen_add(i, diff[i]);		
+	}
+	
+	int num, a, b;
+	for(int i = 0; i < q; i++)
+	{
+		cin >> num;
+		if(num == 1)
+		{
+			cin >> a >> b >> x;
+			diff[a - 1] += x;
+			diff[b] -= x;
+			fen_add(a - 1, x);
+			fen_add(b, -x);
+		}
+		else 
+		{
+			int x;
+			cin >> x;
+			cout << fen_sum(x - 1) << '\n';
+		}
+	}
+}
