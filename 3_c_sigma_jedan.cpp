2c5ad079f4025fdb8442c4b7e60526f41b0b6df9
Miho Hren <miho.hren@fer.hr>
2020-10-25 09:44:43 +0100

    works too slow, dont know how to speed up

--- 3_c_sigma_jedan.cpp
+++ 3_c_sigma_jedan.cpp
@@ -1,84 +1,72 @@
 #include<bits/stdc++.h>
-
-#define LOG(x) cout << #x << "::" << x << '\n'
-#define INIT() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
-#define FORI(n) for(int i = 0; i < n; i++)
-#define FORJ(n) for(int j = 0; j < n; j++)
-#define FORK(n) for(int k = 0; k < n; k++)
+#define LOG(x) cout << #x << '=' << x << '\n'
 using namespace std;
-using ull = uint64_t;
-using ll  = int64_t;
-using i   = int32_t;
-using ss  = int16_t;
-using us  = uint16_t;
+using ll = uint64_t;
+using i = uint32_t;
+void io_init();
+void fill_divs();
 
-using vi   = vector<i>;
-using vll  = vector<ll>;
-using vull = vector<ull>;
-using pii = pair<i,i>;
-using pll = pair<ll, ll>;
+constexpr int n = 1e6;
+i a, b, c;
+ll u,  v;
 
-int q;
-int a, b;
-ll u, v;
+auto divs = vector<ll>(n);
 
-#define N 100001
-int num[N];
-
-auto pairs = vector<pair<ll, ll>>(N); // pair of a number n, and the number of his divisors
-void napuni_djelitelje()
-{
-    for(int i = 1; i <= N; i++)
-    {
-        for(int j = i; j < N; j += i)
-        {
-            num[j - 1] += i;
-        }
-    }
-
-    for(int i = 1; i <= N; i++)
-    {
-        pairs[i - 1] = {i, num[i-1]};
-    }
-
-    sort(pairs.begin(), pairs.end(), [](pll& a, pll& b){ 
-        return a.second == b.second ? a.first < b.first : a.second < b.second;
-    });
-}
 
 int main()
 {
-    INIT();
-    cin >> q;
-    napuni_djelitelje();
+    io_init();
 
+    int q;
+    cin >> q;
     while(q--)
     {
         cin >> a >> b >> u >> v;
-        
-        auto begin = lower_bound(pairs.begin(), pairs.end(), make_pair(0, u), [](const pii& a, const pii& b){
-            return a.second == b.second ? a.first < b.first : a.second < b.second;
-        });
-        
-        auto end = upper_bound(pairs.begin(), pairs.end(), make_pair(0, v), [](const pii& a, const pii& b){
-            return a.second == b.second ? a.first < b.first : a.second < b.second;
-        }); 
-
-        int c = 0;
-
-        while(true)
+        fill_divs(a, b);
+        c = 0;
+        for(int i = a; i <= b; i++)
         {
-            auto curr = *begin;
-            if(begin == end)
-            {
-                if(curr.second <= v && curr.second >= u && curr.first <= b && curr.second >= a)
-                    c++;
-                break;
-            }
-            if(curr.first >= a && curr.first <= b) c++;
-            begin++;
+            if(divs[i - 1] <= v && divs[i - 1] >= u) c++;
         }
 
         cout << c << '\n';
     }
+}
+
+void fill_divs(i a, i b)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = a; j <= b; j += i)
+        {
+            divs[j - 1] += i;
+        }
+    }
+}
+
+/*
+
+Dano je q upita oblika:
+
+"Za koliko prirodnih brojeva n takvih da a⩽n⩽b vrijedi u⩽σ1(n)⩽v?
+
+Ako znate da je σ1(n) zbroj pozitivnih djelitelja n, odgovorite na njih.
+
+Input
+U prvom retku ulaza je 1⩽q⩽105, broj upita.
+
+U sljedećih q redaka, odvojeni razmakom, su redom prirodni brojevi a,b,u i v iz opisa. 
+U svim upitima vrijedi 1⩽a⩽b⩽106 i 1⩽u⩽v⩽1012.
+
+Output
+Ispišite q redaka, u i-tom od kojih je odgovor na i-ti upit.
+
+*/
+
+
+void io_init()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 }
