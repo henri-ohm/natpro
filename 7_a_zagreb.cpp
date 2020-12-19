d9ea5cc3862c14bc8908bd46d555e78ce7c766b0
Miho Hren <miho.hren@fer.hr>
2020-12-19 18:33:40 +0100

    solved 4 real, negdje je previse brojao pa je trebalo koristiti polje

--- 7_a_zagreb.cpp
+++ 7_a_zagreb.cpp
@@ -1,68 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define DBG
-
-#ifdef DBG
-#define DEBUG(x) cout << #x << ' ' << x << '\n'
-#elif
-#define DEBUG(x)
-#endif
 int n, m;
 int n_artikulacijskih = 0;
 int t = 0;
-bool first = true;
-void dfs(int x, vector<int>& dt, vector<int>& min_dt, vector<bool>& visited, int graf[], int depth)
+
+vector<int> dt;
+vector<int> min_dt;
+vector<bool> visited;
+vector<vector<int>> adj;
+vector<bool> ap;
+
+void dfs(int x, int p = -1)
 {
-    if(visited[x]) return;
-
-    dt[x] = t; t++;
     visited[x] = true;
-    int dfs_children = 0;
+    dt[x] = min_dt[x] = t++;
 
-    for(int i = 0; i < n; i++)
+    int children = 0;
+
+    for(auto i: adj[x])
     {
-        if(graf[x + i*n] == 1)
-        {
-            if(visited[i]){
-                min_dt[x] = min(i, min_dt[x]);
-            } else {
-                dfs_children++;
-                dfs(i, dt, min_dt, visited, graf, depth+1);
-                min_dt[x] = min(min_dt[x], min_dt[i]);
-                if(depth != 0 && dt[x] <= min_dt[i]) n_artikulacijskih++;
+        if(i == p) continue;
+        if(visited[i]){
+            min_dt[x] = min(min_dt[x], dt[i]);
+        } else {
+            dfs(i, x);
+            min_dt[x] = min(min_dt[x], min_dt[i]);
+            if(min_dt[i] >= dt[x] && p != -1){
+                //n_artikulacijskih++;
+                ap[x] = true;
             }
+            
+            children++;
         }
     }
-    // DEBUG(x);
-    // DEBUG(dt[x]);
-    // DEBUG(min_dt[x]);
-    // cout << '\n';
-    if(depth == 0 && dfs_children > 1) n_artikulacijskih++;
+
+    if(p == -1 && children > 1) {
+        ap[x] = true;
+        //n_artikulacijskih++;
+    }
 }
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     cin >> n >> m;
     
-    auto dt = vector<int>(n, 0);
-    int val = n+1;
-    auto min_dt = vector<int>(n, val);
-    auto visited = vector<bool>(n, false);
-    int graf[n*n];
-
-    
-
+    dt = vector<int>(n, -1);
+    min_dt = vector<int>(n, -1);
+    visited = vector<bool>(n, false);
+    adj = vector<vector<int>>(n);
+    ap = vector<bool>(n, false);
     int x,y;
     while(m--){
         cin >> x >> y;
         x--; y--;
-        graf[x + y*n] = 1;
-        graf[y + x*n] = 1;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
 
-    
-    dfs(0, dt, min_dt, visited, graf, 0);
+    for(int i = 0; i < n; i++)
+        if(!visited[i])
+            dfs(i);
+
+    // for(int i = 0; i < n; i++)
+    //     if(ap[i]) n_artikulacijskih++;
 
     cout << n_artikulacijskih << '\n';
 }
